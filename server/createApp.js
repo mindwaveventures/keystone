fbeadbdb186d15c9b23f65f7f103d46e4e06c5af
selva5319 <66726566+selva5319@users.noreply.github.com>
2020-08-20 13:57:36 +0530

    update Content-Security-Policy & Feature-Policy

--- server/createApp.js
+++ server/createApp.js
@@ -18,12 +18,14 @@ module.exports = function createApp (keystone, express) {
 	
 	//Intialize the header for secuirty issues in keystone
 	app.use(function (req, res, next) {
-		res.header('X-Content-Type-Options', 'nosniff');
-    	res.header('Referrer-Policy', 'no-referrer');
-    	res.header('X-Permitted-Cross-Domain-Policies', 'none');
+	  res.header('X-Content-Type-Options', 'nosniff');
+    res.header('Referrer-Policy', 'no-referrer');
+    res.header('X-Permitted-Cross-Domain-Policies', 'none');
 		res.header('X-Frame-Options', 'SAMEORIGIN');
 		res.header('X-XSS-Protection', '0');
 		res.header('Strict-Transport-Security', ' max-age=31536000');
+		res.setHeader("Content-Security-Policy", "default-src 'self';base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests");
+		res.header('Feature-Policy', 'none');
 		res.removeHeader('X-Powered-By');
 		res.removeHeader('Server');
 		next()
